daedc3b8dd97c6691fdf6938b995d73fc40f6159
Arshia-Izadyar <arshiaa104@gmail.com>
2023-09-10 21:04:30 +0330

    fixed typo

--- src/utils/csv.py
+++ src/utils/csv.py
@@ -1,10 +1,12 @@
+import time
 import pandas as pd
 from datetime import datetime
 
 
 def export_csv(data):
     print("Creating CSV ...")
+    time.sleep(1)
     df = pd.DataFrame(data)
     csv_file = "jobs_{}.csv".format(datetime.now().date())
     df.to_csv(csv_file, index=False)
-    print("Done !!!")
+    print("Done Create")
